789f122d8c224dfdcfae310e7bce157c3e0a1c4b
Kyryl <kirill_zp@mail.ua>
2020-03-24 17:07:45 +0200

    First commit

--- src/components/FinishedOne/FinishedOne.js
+++ src/components/FinishedOne/FinishedOne.js
@@ -1,5 +1,6 @@
 import React from "react"
 import classes from './FinishedOne.css'
+import Button from "../UI/Button/Button"
 
 const FinishedOne = props =>{
     const successCount = Object.keys(props.results).reduce((total, key) => {
@@ -30,7 +31,8 @@ const FinishedOne = props =>{
             </ul>
             <p>Правильно {successCount} из {props.one.length}</p>
             <div>
-                <button onClick={props.onRetry}>Повторить</button>
+                <Button onClick={props.onRetry} type="primary">Повторить</Button>
+                <Button type="success">Перейти в список тестов</Button>
             </div>
         </div>
     )
